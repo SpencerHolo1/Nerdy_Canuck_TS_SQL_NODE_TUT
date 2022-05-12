f3f4a011179687a07b2e5d75ff6944b354f8b253
Spencer K <skkode35@gmail.com>
2022-05-11 17:45:04 -0700

    Going to find a different tut for now.

--- source/server.ts
+++ source/server.ts
@@ -7,5 +7,26 @@ import config from './config/config';
 const NAMESPACE = 'Server';
 const router = express();
 
-// Left off at video 1/8 11:45
-// Finish this series. Really good at a great pace.
+/** Logging the request */
+router.use((req, res, next) => {
+    logging.info(NAMESPACE, `METHOD - [${req.method}], URL - [${req.url}], [${req.socket.remoteAddress}]`);
+
+    res.on('finish', () => {
+        logging.info(NAMESPACE, `METHOD - [${req.method}], URL - [${req.url}], [${req.socket.remoteAddress}], STATUS - [${res.statusCode}]`);
+    });
+});
+
+/** Parse the request. This allows us to send nested JSON to our API */
+router.use(bodyParser.urlencoded({ extended: false }));
+/** Allows us to not have to use 'json.parse' or 'json.stringify' on the React side of things. We just take care of that here */
+router.use(bodyParser.json());
+
+/** Rules of our API */
+router.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+    if ((req.method = 'OPTIONS')) {
+        res.header('Access-Control-Allow-Methods', 'GET PATCH DELETE');
+    }
+});
